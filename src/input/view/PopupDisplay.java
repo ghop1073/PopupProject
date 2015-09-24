501a5b9a4e80d4932ecace434859ae0b62a20469
ghop1073 <mhop1073@740s-pl-109.local>
2015-09-24 12:36:24 -0600

    add display pane for display

--- src/input/view/PopupDisplay.java
+++ src/input/view/PopupDisplay.java
@@ -1,12 +1,24 @@
 package input.view;
 
+import javax.swing.JOptionPane;
+/**
+ * This class provides popups for input and output.
+ * @author mhop1073
+ */
 public class PopupDisplay
 {
 
 	public String getAnswer(String input)
 	{
 		String answer = "";
-				
+			
+		answer = JOptionPane.showInputDialog(null, input);
+		
 		return answer;
+	}
+	
+	public void displayResponse(String input)
+	{
+		JOptionPane.showMessageDialog(null, input);
 	}
 }
